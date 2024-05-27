b00f55b032831723926205436d343bb5b0cd6056
Alexis Orellana <od18003@ues.edu.sv>
2024-05-26 19:17:55 -0600

    creacion proyecto

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProyectoInnovacionPDM2024-GT01-Grupo3"
+include(":app")
+ 
