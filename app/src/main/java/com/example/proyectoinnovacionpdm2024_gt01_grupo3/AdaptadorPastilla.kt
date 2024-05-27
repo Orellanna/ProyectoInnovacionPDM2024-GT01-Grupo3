c9875eb6b3aa7b9592c87627b1425fe2f1676902
Alexis Orellana <od18003@ues.edu.sv>
2024-05-26 19:34:12 -0600

    Add Main Activity y Adapter

---
+++ app/src/main/java/com/example/proyectoinnovacionpdm2024_gt01_grupo3/AdaptadorPastilla.kt
@@ -0,0 +1,33 @@
+package com.example.proyectoinnovacionpdm2024_gt01_grupo3
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PillAdapter(private val pills: List<Pill>) : RecyclerView.Adapter<PillAdapter.PillViewHolder>() {
+
+    class PillViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.textViewName)
+        val descriptionTextView: TextView = itemView.findViewById(R.id.textViewDescription)
+        val datesTextView: TextView = itemView.findViewById(R.id.textViewDates)
+        val frequencyTextView: TextView = itemView.findViewById(R.id.textViewFrequency)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PillViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.pill_item, parent, false) // Cambiado de pill_item a item_pill
+        return PillViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PillViewHolder, position: Int) {
+        val currentPill = pills[position]
+        holder.nameTextView.text = currentPill.name
+        holder.descriptionTextView.text = currentPill.description
+        holder.datesTextView.text = "Desde ${currentPill.startDate} hasta ${currentPill.endDate}"
+        holder.frequencyTextView.text = "Cada ${currentPill.frequency} horas a las: ${currentPill.startTime}"
+    }
+
+    override fun getItemCount() = pills.size
+}
--- app/src/main/java/com/example/proyectoinnovacionpdm2024_gt01_grupo3/MainActivity.kt
+++ app/src/main/java/com/example/proyectoinnovacionpdm2024_gt01_grupo3/MainActivity.kt
@@ -1,11 +1,136 @@
 package com.example.proyectoinnovacionpdm2024_gt01_grupo3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSION_REQUEST_CODE = 123
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var pillAdapter: PillAdapter
+    private lateinit var dbHelper: ConexionDataBaseHelper
+    private val CHANNEL_ID = "pill_reminder_channel"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        requestPermission()
+
+        createNotificationChannel()
+
+        dbHelper = ConexionDataBaseHelper(this)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, AddPastilla::class.java)
+            startActivity(intent)
+        }
+
+        loadPills()
+
+    }
+
+    private fun loadPills() {
+        val db = dbHelper.readableDatabase
+        val projection = arrayOf(
+            ConexionDataBaseHelper.COLUMN_ID,
+            ConexionDataBaseHelper.COLUMN_NAME,
+            ConexionDataBaseHelper.COLUMN_DESCRIPTION,
+            ConexionDataBaseHelper.COLUMN_START_DATE,
+            ConexionDataBaseHelper.COLUMN_END_DATE,
+            ConexionDataBaseHelper.COLUMN_FREQUENCY,
+            ConexionDataBaseHelper.COLUMN_START_TIME
+        )
+
+        val cursor = db.query(
+            ConexionDataBaseHelper.TABLE_PILLS,
+            projection,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+
+        val pills = mutableListOf<Pill>()
+
+        with(cursor) {
+            while (moveToNext()) {
+                val id = getLong(getColumnIndexOrThrow(ConexionDataBaseHelper.COLUMN_ID))
+                val name = getString(getColumnIndexOrThrow(ConexionDataBaseHelper.COLUMN_NAME))
+                val description = getString(getColumnIndexOrThrow(ConexionDataBaseHelper.COLUMN_DESCRIPTION))
+                val startDate = getString(getColumnIndexOrThrow(ConexionDataBaseHelper.COLUMN_START_DATE))
+                val endDate = getString(getColumnIndexOrThrow(ConexionDataBaseHelper.COLUMN_END_DATE))
+                val frequency = getInt(getColumnIndexOrThrow(ConexionDataBaseHelper.COLUMN_FREQUENCY))
+                val startTime = getString(getColumnIndexOrThrow(ConexionDataBaseHelper.COLUMN_START_TIME))
+                pills.add(Pill(id, name, description, startDate, endDate, frequency, startTime))
+            }
+        }
+
+        pillAdapter = PillAdapter(pills)
+        recyclerView.adapter = pillAdapter
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            val existingChannel = notificationManager.getNotificationChannel(CHANNEL_ID)
+            if (existingChannel == null) {
+                val name = "Pill Reminder"
+                val descriptionText = "Channel for Pill Reminder notifications"
+                val importance = NotificationManager.IMPORTANCE_HIGH
+                val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                    description = descriptionText
+                }
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Permiso concedido
+            } else {
+                // Permiso denegado, manejar el caso
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadPills()
     }
 }
